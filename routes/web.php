20e8a10e4b2e829e23a1d5b7a4e80a63f1c8b35d
Asad <asadrifat450@gmail.com>
2025-12-20 23:00:05 +0600

    design inplement

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
+use App\Http\Controllers\Home\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::middleware('auth')->group(function () {
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/',[HomeController::class,'dashboard'])->name('dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
